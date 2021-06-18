cc8c89ded0e3718dff12601244cf41140b3c6838
eshan1799 <eshan1799@gmail.com>
2021-06-18 20:36:26 +0100

    add get to / route

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const port = process.env.PORT || 5000
 server.use(express.json())
 server.use(cors())
 
-const corsOptions = {
-    origin: 'https://notion-api-sdk.herokuapp.com/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// const corsOptions = {
+//     origin: 'https://notion-api-sdk.herokuapp.com/',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-server.post('/', cors(corsOptions), function (req, res) {
+server.get('/', (req, res) => { res.send("Notion server running!") })
+
+server.post('/', function (req, res) {
     const data = req.body
     const newPageRes = createNewPage(data.postWord, data.postDefinition, data.postSynonyms)
     res.send(newPageRes);
